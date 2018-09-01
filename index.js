5f66e3cfe0996b82f368e1defa8d2fb5c6fc36e1
dalia <sawaya.dalia@gmail.com>
2018-09-01 14:18:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 // Write your solution in this file!
 
-var customerName = 'bob';
 
-const leastFavoriteCustomer = 'sour fred';
 
 function upperCaseCustomerName () {
-  return customerName = customerName.toUpperCase();
+
 }
 
 
 
 function upperCaseCustomerName () {
-  return customerName = customerName.toUpperCase();
+
 }
 
 
 
 function changeLeastFavoriteCustomer () {
-  leastFavoriteCustomer = 'sour fred';
+
 }
 
 function setBestCustomer () {
-  bestCustomer = 'not bob';
+
 }
 
 function overwriteBestCustomer (newFavorite) {
